7e23764376485ec721afe20e42c783d7cb806c29
arjunarj <nagarjunan66@gmail.com>
2022-08-24 10:49:09 +0530

    Home screen changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,97 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home'));
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage("images/bg.png"),
+        fit: BoxFit.cover,
+      )),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: ListView(
+          children: [
+            SizedBox(height: 50),
+            Text(
+              'Welcome!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'User',
+              style: TextStyle(fontSize: 36),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14)),
+              elevation: 5,
+              child: Container(
+                color: Colors.deepPurple[100],
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '\"The way I see it, if you want the rainbow, you gotta put up with the rain.\"',
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize: 22,
+                              color: Colors.deepPurple[900]),
+                        ),
+                        Text(
+                          '\n-Dolly Parton',
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.deepPurple[900]),
+                        ),
+                      ]),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text("Evaluate yourself",
+                style: TextStyle(
+                  fontSize: 28,
+                )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style:
+                          ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                      onPressed: () {},
+                      child: Text('Stress Test')),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                      style:
+                          ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                      onPressed: () {},
+                      child: Text('Anxiety Test')),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Text('Favorites',
+                style: TextStyle(
+                  fontSize: 28,
+                )),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    ));
   }
 }
