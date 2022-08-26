dcdc686863d423b7ae7491add539e3188739b081
arjunarj <nagarjunan66@gmail.com>
2022-08-26 17:06:06 +0530

    Added Video Player

--- lib/streamer/player.dart
+++ lib/streamer/player.dart
@@ -37,39 +37,120 @@ class _PlayerState extends State<Player> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 14, 11, 18),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            FutureBuilder(
-              future: _initializeVideoPlayerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  );
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Stack(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Colors.deepPurple[100],
+                  size: 28,
+                ),
+              ),
             ),
-            SizedBox(height: 20),
-            // Text("${_controller.value.isPlaying}"),
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  _controller.value.isPlaying
-                      ? _controller.pause()
-                      : _controller.play();
-                });
-              },
-              icon: Icon(
-                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
-            )
-          ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(14),
+                    child: FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return AspectRatio(
+                            aspectRatio: _controller.value.aspectRatio,
+                            child: VideoPlayer(_controller),
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+                  child: VideoProgressIndicator(_controller,
+                      allowScrubbing: true,
+                      colors: VideoProgressColors(
+                        backgroundColor: Color.fromRGBO(209, 196, 233, 1),
+                        playedColor: Colors.deepPurple,
+                      )),
+                ),
+                SizedBox(height: 20),
+                // Text("${_controller.value.isPlaying}"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          _controller.seekTo(Duration(
+                              seconds:
+                                  _controller.value.position.inSeconds - 2));
+                        },
+                        icon: Icon(
+                          Icons.fast_rewind_rounded,
+                          color: Colors.deepPurple[100],
+                          size: 36,
+                        )),
+                    SizedBox(width: 10),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _controller.value.isPlaying
+                              ? _controller.pause()
+                              : _controller.play();
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.deepPurple[100],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Icon(
+                              _controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow_rounded,
+                              color: Color.fromARGB(255, 14, 11, 18),
+                              size: 42,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    IconButton(
+                        onPressed: () {
+                          _controller.seekTo(Duration(
+                              seconds:
+                                  _controller.value.position.inSeconds + 2));
+                        },
+                        icon: Icon(
+                          Icons.fast_forward_rounded,
+                          color: Colors.deepPurple[100],
+                          size: 36,
+                        )),
+                  ],
+                )
+              ],
+            ),
+          ]),
         ),
       ),
     );
